37cead5a08ed35914cd478108567a1129e13e017
Nemezix <38551998+Nemezix@users.noreply.github.com>
2018-05-03 15:36:15 +0200

    Update members.php

--- phProjo/views/members.php
+++ phProjo/views/members.php
@@ -1,12 +1,8 @@
-		<div id="webdevelopper">
-			<span>Vue : Arthur // Controller : Arthur&Roman</span>
-		</div>
-
-		<section>
-			<h1>members 6</h1>
-			<p>liste des membres</p>
-			<p>redirige vers : hub, profil, training, events, members.php et logout</p>
-			<!-- must be hidden on authentification / if admin -->
-			<div id="admin">Espace d'administration pour les MODÉRATEURS</div>
-		</section>
-
+<div class="webdevelopper">Vue : Arthur // Controller : Arthur&Roman</div>
+<section>
+<h1>members 6</h1>
+<p>liste des membres</p>
+<p>redirige vers : hub, profil, training, events, members.php et logout</p>
+<!-- must be hidden on authentification / if admin -->
+<div class="admin">Espace d'administration pour les MODÉRATEURS</div>
+</section>
