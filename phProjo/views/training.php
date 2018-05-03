837fc53c4b57b3c2fb860f9ee8af9bdc8ec4b57b
Nemezix <38551998+Nemezix@users.noreply.github.com>
2018-05-03 15:40:13 +0200

    Update training.php

--- phProjo/views/training.php
+++ phProjo/views/training.php
@@ -1,11 +1,9 @@
-		<div id="webdevelopper">
-			<span>Vue : Arthur // Controller : Arthur</span>
-		</div>
+<div class="webdevelopper">Vue : Arthur // Controller : Arthur</div>
 
-		<section>
-			<h1>titre 4</h1>
-			<p>la liste des entrainements</p>
-			<p>redirige vers : hub, profil, training, events, members.php et logout</p>
-			<!-- must be hidden on authentification / if admin -->	
-			<div id="coach">Espace d'administration pour le COACH</div>
-		</section>
+<section>
+<h1>titre 4</h1>
+<p>la liste des entrainements</p>
+<p>redirige vers : hub, profil, training, events, members.php et logout</p>
+<!-- must be hidden on authentification / if admin -->	
+<div class="coach">Espace d'administration pour le COACH</div>
+</section>
